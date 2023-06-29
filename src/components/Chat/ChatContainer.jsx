e9d09c9ecab3b7bcf2f885a41e6a60e9cee9f328
Redwi-v <83034070+Redwi-v@users.noreply.github.com>
2023-06-29 11:33:29 +0300

    Update ChatContainer.jsx

--- src/components/Chat/ChatContainer.jsx
+++ src/components/Chat/ChatContainer.jsx
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client';
 const ChatContainer = (props) => {
   const socket = useRef();
   useEffect(() => {
-    socket.current = io('https://my-chat112.herokuapp.com');
+    socket.current = io('http://localhost:7777');
   }, []);
 
   const {
